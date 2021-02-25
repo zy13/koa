3fd675a53a928ef318f4596ab21e5c9d967d880e
zy <zengyuan@keywa.com>
2021-02-25 15:34:51 +0800

    添加练习

--- README.md
+++ README.md
@@ -1,23 +1,39 @@
-# koa
+# koa with mysql
+## nunjucks模板引擎的应用
 
-* package.json
-```
-通过命令npm init生成，用于描述项目名称、版本、作者、相关依赖等信息，其中scripts字段为脚本运行的命令行定义，dependencies为项目运行所需要的依赖，devDependencies为项目开发运行所需的依赖
-```
+- 服务端引用nunjuck库
+  - nunjuck.configure配置
+  - nunjuck.render()
+  - 二次封装nunjucks，并挂载到ctx.render(tplName, data)：简化代码，便于维护
+- 模板文件渲染
+  - include引入公共模块：公共头部和尾部
+  - extends扩展基础模块
+  - block引入有差别的模块
+  - 大胡子语法渲染数据
 
-* npm i module-name --save-dev
-```
-安装模块，项目开发过程中使用的模块，并保存模块信息到package.json中的devDependencies
-```
+## mysql数据库的应用
+- 第三方库mysql2的引入
+- 使用单例模式，二次封装mysql，并挂载到ctx.connection
+- 连接数据库，创建数据库，创建表格
+- 实现数据的查询、增加
 
-* npm run dev: 开启服务端
+## 动态路由
+- 客户端发生发送数据的方式：1.url； 2.请求头；3.请求正文
+- router.post实现动态路由，并将数据挂载到`ctx.request.params`
 
-* gitignore
-```
-代码提交时，需要过滤掉的文件
-```
+## post请求的数据处理
+- 客户端
+  - 通过form表单的post请求方式提交数据
+- koa-body中间件
+  koa-body解析post请求的数据，并挂载到`ctx.request.body`
+- 将客户端的数据存到数据库中
 
-* app.js
-```
-项目启动入口文件
-```
+## 练习
+- 1、创建一个数据库kkb 
+- 2、在kkb数据库中创建一个表users 
+- 3、在users表中添加至少下面两个字段： 
+  - 1、id(int)，username(varchar) 
+- 4、通过koa、koa-router构建一个webserver，端口8888 
+- 5、提供一个get方式接口localhost:8888/register访问注册页面（form表单） 
+- 6、提供一个post方式接口localhost:8888/register处理提交的数据，并返回操作结果（注册失败 or 注册成功） 
+- 7、提交注册的数据保存到数据库users表中 通过标准：完成以上所有任务及要求，方可通过。
--- app.js
+++ app.js
@@ -1,19 +1,13 @@
 const Koa = require('koa')
-const koaStaticCache = require('koa-static-cache')
 const KoaRouter = require('koa-router')
+const koaStaticCache = require('koa-static-cache')
 const koaBody = require('koa-body')
-const koaNunjucks = require('./middlewares/koa-nunjucks')
 const koaConnection = require('./middlewares/koa-connection')
+const koaNunjucks = require('./middlewares/koa-nunjucks')
 
 const app = new Koa()
 const router = new KoaRouter()
 
-// 注册nunjucks
-app.use(koaNunjucks())
-// 连接数据库
-app.use(koaConnection())
-
-// 静态资源代理
 app.use(koaStaticCache({
   prefix: '/public',
   dir: './public',
@@ -21,69 +15,26 @@ app.use(koaStaticCache({
   dynamic: true
 }))
 
-// 首页
-router.get('/', async ctx => {
-  const [categories] = await ctx.connection.query(
-    'select * from categories'
-  )
-  const [items] = await ctx.connection.query(
-    'select * from items limit 10'
-  )
-  console.log(categories)
-  ctx.body = ctx.render('index', {
-    title: '首页',
-    categories,
-    items
-  })
-})
+app.use(koaNunjucks())
+app.use(koaConnection())
 
-// 列表页
-router.get('/list/:categoryId', async ctx => {
-  // 因为url在请求过程中再携带一些动态的额外数据，这个数据会影响当前从数据库中查询的结果
-  // 客户端发送请求可以发送数据：1.url 2.请求头 3.请求正文
-  // koa-router 会在分析当前动态url时候，把/list/1或者/list/2后面:匹配的内容单独解析出来，
-  // 然后存放到ctx.request.params.categoryId
-  let { categoryId } = ctx.request.params
+router.get('/register', async ctx => {
   const [categories] = await ctx.connection.query(
     'select * from categories'
   )
-  const [items] = await ctx.connection.query(
-    'select * from items where category_id=?',
-    [categoryId]
-  )
-  ctx.body = ctx.render('list', {
-    title: '列表',
-    categories,
-    items
-  })
-})
-
-// 详情页
-router.get('/detail/:detailId', async ctx => {
-  let { detailId } = ctx.request.params
-  const [categories] = await ctx.connection.query(
-    'select * from categories'
-  )
-  const [[item]] = await ctx.connection.query(
-    'select * from items where id=?',
-    [detailId]
-  )
-  ctx.body = ctx.render('detail',{
-    title: '详情页',
-    item,
+  ctx.body = ctx.render('register',{
+    title: '注册',
     categories
   })
 })
 
-// 数据量比较大的请求，使用post提交数据
-router.post('/comment', koaBody(), async ctx => {
-  // 默认情况下，koa不会解析post提交的正文请求中的数据，其解析头部和url的信息
-  let { content, detailId } = ctx.request.body
-  let [rs] = await ctx.connection.query(
-    'insert into comments (content,datatime,detail_id) values (?,?,?)',
-    [content,Date.now(),detailId]
+router.post('/register', koaBody(), async ctx => {
+  const { username, password } = ctx.request.body
+  await ctx.connection.query(
+    'insert into users (username, password) values (?,?)',
+    [username, password]
   )
-  ctx.body = '评论成功'
+  ctx.body = '注册成功'
 })
 
 app.use(router.routes())
