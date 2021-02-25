b91beb0d4343575152b7350d498035276f333915
zy <zengyuan@keywa.com>
2021-02-25 18:24:28 +0800

    数据库操作
    1、排序
    2、分页

--- README.md
+++ README.md
@@ -1,39 +1,6 @@
-# koa with mysql
-## nunjucks模板引擎的应用
+## dayjs插件
+- moment插件的精简版，将时间戳进行格式化
+## 数据库操作
+- 限制数据的显示条数
+- 降序排序，升序排序
 
-- 服务端引用nunjuck库
-  - nunjuck.configure配置
-  - nunjuck.render()
-  - 二次封装nunjucks，并挂载到ctx.render(tplName, data)：简化代码，便于维护
-- 模板文件渲染
-  - include引入公共模块：公共头部和尾部
-  - extends扩展基础模块
-  - block引入有差别的模块
-  - 大胡子语法渲染数据
-
-## mysql数据库的应用
-- 第三方库mysql2的引入
-- 使用单例模式，二次封装mysql，并挂载到ctx.connection
-- 连接数据库，创建数据库，创建表格
-- 实现数据的查询、增加
-
-## 动态路由
-- 客户端发生发送数据的方式：1.url； 2.请求头；3.请求正文
-- router.post实现动态路由，并将数据挂载到`ctx.request.params`
-
-## post请求的数据处理
-- 客户端
-  - 通过form表单的post请求方式提交数据
-- koa-body中间件
-  koa-body解析post请求的数据，并挂载到`ctx.request.body`
-- 将客户端的数据存到数据库中
-
-## 练习
-- 1、创建一个数据库kkb 
-- 2、在kkb数据库中创建一个表users 
-- 3、在users表中添加至少下面两个字段： 
-  - 1、id(int)，username(varchar) 
-- 4、通过koa、koa-router构建一个webserver，端口8888 
-- 5、提供一个get方式接口localhost:8888/register访问注册页面（form表单） 
-- 6、提供一个post方式接口localhost:8888/register处理提交的数据，并返回操作结果（注册失败 or 注册成功） 
-- 7、提交注册的数据保存到数据库users表中 通过标准：完成以上所有任务及要求，方可通过。
--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const koaStaticCache = require('koa-static-cache')
 const koaBody = require('koa-body')
 const koaConnection = require('./middlewares/koa-connection')
 const koaNunjucks = require('./middlewares/koa-nunjucks')
+const dayjs = require('dayjs')
 
 const app = new Koa()
 const router = new KoaRouter()
@@ -17,6 +18,90 @@ app.use(koaStaticCache({
 
 app.use(koaNunjucks())
 app.use(koaConnection())
+
+router.get('/', async ctx => {
+  const [categories] = await ctx.connection.query(
+    'select * from categories'
+  )
+  const [items] = await ctx.connection.query(
+    'select * from items limit 10'
+  )
+  ctx.body = ctx.render('index', {
+    title: '首页',
+    categories,
+    items
+  })
+})
+
+router.get('/list/:categoryId', async ctx => {
+  const { categoryId } = ctx.request.params
+  const [categories] = await ctx.connection.query(
+    'select * from categories'
+  )
+  const [items] = await ctx.connection.query(
+    'select * from items where category_id=?',
+    [categoryId]
+  )
+  ctx.body = ctx.render('list', {
+    title: '列表',
+    categories,
+    items
+  })
+})
+
+router.get('/detail/:detailId', async ctx => {
+  const { detailId } = ctx.request.params
+  let { page } = ctx.request.query
+  page = page ? Number(page) : 1
+  const [categories] = await ctx.connection.query(
+    'select * from categories'
+  )
+  const [[item]] = await ctx.connection.query(
+    'select * from items where id=?',
+    [detailId]
+  )
+  
+  let limit = 5
+  // 第一页：offset=0；第二页：offset = 5; 
+  // 第page页：offset = limit * (page-1)
+  let offset = limit * (page-1)
+  // 排序：order by 字段1 排序方式，字段2 排序方式
+  // 排序方式：desc => 降序, asc => 升序
+  // 分页借助：limit offset
+  // limit 条数 => 限制查询多少条
+  // offset 条数 => 从第几条开始，默认从0
+  let [comments] = await ctx.connection.query(
+    'select * from comments where detail_id=? order by id desc limit ? offset ?',
+    [detailId,limit,offset]
+  )
+  comments = comments.map(item => {
+    return {
+      ...item,
+      formateDatetime: dayjs(item.datetime).format('YYYY年MM月DD日 HH:mm:ss')
+    }
+  })
+  ctx.body = ctx.render('detail', {
+    title: '详情',
+    detailId,
+    categories,
+    item,
+    comments
+  })
+})
+
+router.post('/comment', koaBody(), async ctx => {
+  const { content, detailId } = ctx.request.body
+  const [categories] = await ctx.connection.query(
+    'select * from categories'
+  )
+  const [rs] = await ctx.connection.query(
+    'insert into comments (content, datatime, detail_id) values (?,?,?)',
+    [content, Date.now(), detailId]
+  )
+  ctx.body = ctx.render('message',{
+    categories
+  })
+})
 
 router.get('/register', async ctx => {
   const [categories] = await ctx.connection.query(
