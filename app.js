59a0e135130f65938410c17649d26c0846bc52da
zy <zengyuan@keywa.com>
2021-02-26 16:35:16 +0800

    ajax-无刷新请求数据

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log('app.js')
+const Koa = require('koa')
+const KoaRouter = require('koa-router')
+const koaBody = require('koa-body')
+const koaStaticCache = require('koa-static-cache')
+
+const app = new Koa()
+const router = new KoaRouter()
+
+const users = [
+  {id: 1, name: 'zy-1'},
+  {id: 2, name: 'zy-3'},
+]
+
+app.use(koaStaticCache({
+  prefix: '/public',
+  dir: './public',
+  gzip: true,
+  dynamic: true
+}))
+
+router.get('/user', async ctx => {
+  ctx.body = users
+})
+
+app.use(router.routes())
+app.listen(8888)
