5803a3a712b070707fea83c5bf2c298e80e29f99
zy <zengyuan@keywa.com>
2021-03-01 10:15:49 +0800

    XMLHttpRequest对象

--- app.js
+++ app.js
@@ -22,5 +22,15 @@ router.get('/user', async ctx => {
   ctx.body = users
 })
 
+router.post('/user', koaBody(), async ctx => {
+  // let { username } = ctx.request.params
+  // let { username } = ctx.request.query
+  let { username } = ctx.request.body
+  if(!username) {
+    ctx.throw(400, '用户名不能为空')
+  }
+  ctx.body = '请求成功！'
+})
+
 app.use(router.routes())
 app.listen(8888)
